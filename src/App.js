108b14892c17baeeeb777b9f9a193e9f00d9a6db
Andrea <andiserm@gmail.com>
2023-01-16 21:28:33 -0300

    Agrego solucion al test fallido

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      counter: 0,
+      increment: (e) => {
+        this.setState( { counter: e.target.value.length })
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+      <div className='container'>
+        <textarea rows='3' onChange={this.state.increment}></textarea>
+        <div className='counter'>{this.state.counter}</div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
